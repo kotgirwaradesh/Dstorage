b6fb603db069066f641871d6d74e3243b9a49858
kotgirwaradesh <59340247+kotgirwaradesh@users.noreply.github.com>
2021-09-05 19:48:37 +0530

    Add files via upload

---
+++ build/precache-manifest.e0840c10ff63cadaeafe361e3ad2d51e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14471b9c4c4be8b0328c89889b8c0890",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84d9cbd1089535eef67f",
+    "url": "./static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "4dde7d373fd9d3285dc2",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "84d9cbd1089535eef67f",
+    "url": "./static/js/2.7cf32fbb.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.7cf32fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dde7d373fd9d3285dc2",
+    "url": "./static/js/main.b272e245.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "./static/media/box.df8b84d5.png"
+  }
+]);
